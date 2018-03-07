3c18fd531244d29588a8a0684cc0fc6554aaa876
KenLiHK <kenlicontact@gmail.com>
2018-03-08 03:06:26 +0800

    update addFood substractFood functions

--- init.php
+++
@@ -1,8 +0,0 @@
-<?php
-	//define web application context root path variable
-	define('UNICORN_ROOT', (isset($_SERVER['HTTPS']) ? "https" : "http"). "://".$_SERVER['HTTP_HOST']."/CS5281Unicorn");
-
-	//define document root path variable
-	define('UNICORN_DOC_ROOT', $_SERVER['DOCUMENT_ROOT']."/CS5281Unicorn");
-	
-?>
